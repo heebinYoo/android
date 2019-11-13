565c4d2aaad20f78287b8facd274cc560a3f72e2
cau <cau@cau>
2019-11-13 22:43:55 +0900

    비동기 처리 다른 방법 도서관에서 책 보고 알아내기

--- smartRoute/app/src/main/java/com/heebin/smartroute/init/InitActivity.java
+++ smartRoute/app/src/main/java/com/heebin/smartroute/init/InitActivity.java
@@ -1,25 +1,30 @@
 package com.heebin.smartroute.init;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.heebin.smartroute.Constants;
-import com.heebin.smartroute.InitMapsActivity;
 import com.heebin.smartroute.R;
+import com.heebin.smartroute.bean.userData.UserLocation;
+import com.heebin.smartroute.busAPI.StationSearcher;
 
 public class InitActivity extends AppCompatActivity {
+    Button go;
+    TextView inform;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init);
-        Button go = findViewById(R.id.gobtn1);
+        go = findViewById(R.id.gobtn);
+        inform = findViewById(R.id.init_text_view);
         go.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,21 +39,35 @@ public class InitActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-
         if (resultCode == Constants.init_google_map_ok) {
             if(requestCode == Constants.need_home_loaction_init_google_map) {
                 double latitude = data.getDoubleExtra("latitude", 0);
                 double longitude = data.getDoubleExtra("longitude", 0);
+                UserLocation.getInstance().setHome(latitude, longitude);
+                Log.d("InitActivityHome", "onActivityResult: " +latitude +" "+longitude);
 
-                Log.d("InitActivity", "onActivityResult: " +latitude +" "+longitude);
+                inform.setText("Then, let's set your office");
+                go.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent i = new Intent(InitActivity.this, InitMapsActivity.class);
+                        startActivityForResult(i,Constants.need_office_loaction_init_google_map);
+                    }
+                });
 
             }
             else if(requestCode == Constants.need_office_loaction_init_google_map){
                 double latitude = data.getDoubleExtra("latitude", 0);
                 double longitude = data.getDoubleExtra("longitude", 0);
+                UserLocation.getInstance().setOffice(latitude,longitude);
+                Log.d("InitActivityOffice", "onActivityResult: " +latitude +" "+longitude);
 
-                Log.d("InitActivity", "onActivityResult: " +latitude +" "+longitude);
+                StationSearcher ss = new StationSearcher();//다른 방법 찾아보기
+                AsyncTask.execute(ss);
+
+                finish();
             }
+
         }
     }
 }
